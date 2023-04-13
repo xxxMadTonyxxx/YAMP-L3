746e88f7271bb6cf70ab40b62052ea353e81b549
xxxMadTonyxxx <129593467+xxxMadTonyxxx@users.noreply.github.com>
2023-04-13 12:22:28 +0300

    Add files via upload

--- KIRKA/KIRKA.cpp
+++ KIRKA/KIRKA.cpp
@@ -10,24 +10,22 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
     const int N = 10;
-	int a[N];
-	int i;
-	int k;
-	cout << "Введите 0 если хотите сами ввести значения массива " << endl << "Введите 1 если хотите, чтобы это сделал рандомайзер";
+	int Array[N], i, k;
+	cout << "Введите 0 если хотите сами ввести значения массива " << endl << "Введите 1 если хотите, чтобы это сделал рандомайзер" << endl;
 	cin >> k;
 	if (k == 0)
 	{
 		cout << "Введите " << N << " чисел через пробел" << endl;
-		for (i = 0; i < N; i++)
-			cin >> a[i];
+		for ( i = 0; i < N; i++)
+			cin >> Array[i];
 		cout << "Массив" << endl;
-		for (i = 0; i < N; i++)
-			cout << a[i] << " ";
+		for ( i = 0; i < N; i++)
+			cout << Array[i] << " ";
 		cout << endl;
 		if (i > 2) {
 			cout << "Все элементы массива, кроме первого и последнего" << endl;
 			for (i = 1; i < N - 1; i++)
-				cout << a[i] << " ";
+				cout << Array[i] << " ";
 			cout << endl;
 		}
 		else
@@ -37,15 +35,24 @@ int main()
 	else if (k == 1)
 	{
 		int l;
+		int a[N]{};
 		cout << "Введите свой номер в списке группы" << endl;
 		cin >> l;
 		srand(time(NULL));
-		for (i = 0; i < N; i++)
-			a[N] = rand() % 10 * l + 1;
-		for (i = 0; i < N; i++)
-			cout << a[N] << "";
-		system("pause");
-		return 0;
+		cout << "Массив рандомных чисел:" << endl;
+		for (i = 0; i < N; i++) {
+			a[i] = 1 + rand() % 10 * l;
+			cout << a[i] << " ";
+		}
+		if (i > 2) {
+			cout << "Все элементы массива, кроме первого и последнего" << endl;
+			for (i = 1; i < N - 1; i++)
+				cout << a[i] << " ";
+			cout << endl;
+		}
+		else
+			cout << "Нет элементов, уоторые удовлетворяют условию" << endl << endl;
+		
 	}
 	else
 		cout << "Введены неправильные значения";
